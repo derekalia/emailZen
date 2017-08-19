cddc6d08c7519e6e35b3b53ca2650fa8fd24ed40
Derek Alia <derekalia@me.com>
2017-08-18 21:21:28 -0700

    fixed keys file

--- config/keys.js
+++ config/keys.js
@@ -2,6 +2,7 @@
 
 if (process.env.NODE_ENV === 'production'){
     //we are in pro- return prod set of keys
+    module.exports = require('./prod')
 }else{
     //we are in dev return dev keys
     module.exports = require('./dev')
