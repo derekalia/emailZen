640101d58e316f7c4688a74382c4107020620eb0
Derek Alia <derekalia@me.com>
2017-08-18 21:36:32 -0700

    changed passport file

--- config/prod.js
+++ config/prod.js
@@ -6,17 +6,4 @@ module.exports = {
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY
 }
-
-// module.exports = {
-//     googleClientId: "610283717853-sthsnnva8jmduc9c6v8jbah2fe50q3uf.apps.googleusercontent.com",
-//     googleClientSecret: "vAyNadBcHGLHjhkGZm6D7LrE",
-//     mongoURI: "mongodb://derek:abuh1234@ds027175.mlab.com:27175/emaily-prod-derek",
-//     cookieKey: "kyjghjgkjhblkjhgchdxhgfcklkugkjgv "
-// }
-
-// module.exports = {
-//     googleClientId: "128827921004-bneq1bdmeniqfr1c52kb3qmqseg4nred.apps.googleusercontent.com",
-//     googleClientSecret: "iU2JjN7yT-LQtYthy0F2ubDy",
-//     mongoURI: "mongodb://derek:password@ds149373.mlab.com:49373/emaily-dev",
-//     cookieKey: "asdsfrlksjdfesflskdajhfvbaslkvaslurhsdufsrbhlhusdafhsbl "
-// }
+  
