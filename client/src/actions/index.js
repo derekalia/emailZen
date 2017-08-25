a69e4604ceda03a9b4d0a8c51890a7feb2189ada
Derek Alia <derekalia@me.com>
2017-08-25 15:14:15 -0700

    setup stripe basics

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,13 +1,19 @@
 import axios from 'axios';
-import {FETCH_USER} from './types';
+import { FETCH_USER } from './types';
 
-// const fetchUser = async () => {
-//     const current_user = await axios.get('/api/current_user');   
+//old way using .then
+// export const fetchUser = () => {
+//   return function(dispatch) {
+//     axios.get('/api/current_user').then(res => dispatch({ type: FETCH_USER, payload: res }));
+//   };
 // };
 
-export const fetchUser = () => {
-    return function(dispatch){
-     axios.get('/api/current_user')
-     .then(res => dispatch({type:FETCH_USER, payload: res}))
-    }
+// using async
+export const fetchUser = () => async dispatch => {
+  const current_user = await axios.get('/api/current_user');
+  dispatch({ type: FETCH_USER, payload: current_user.data });
 };
+
+// //code golf
+// export const fetchUser = () => async dispatch =>
+//   dispatch({ type: FETCH_USER, payload: await axios.get('/api/current_user') });
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-export default class Header extends React.Component {
+class Header extends React.Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return 'still deciding';
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
   render() {
+    console.log(this.props);
+
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">Emaily</a>
+          <Link to={this.props.auth ? '/surveys' : '/'} className="left brand-logo">
+            Emaily
+          </Link>
           <ul className="right">
-            <li>
-              <a>Login with google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
         Header
@@ -17,3 +39,8 @@ export default class Header extends React.Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+export default connect(mapStateToProps)(Header);
