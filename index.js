ddd6fb8426ec44ee08a87626a26a7dda8aa6c52e
Derek Alia <derekalia@me.com>
2017-08-26 15:29:42 -0700

    error in server/index

--- index.js
+++ index.js
@@ -32,9 +32,9 @@ if(process.env.NODE_ENV === 'production'){
 
   //expess will serve up the index.html file if it doenst recognice the route
   const path = require('path')
-  app.get('*', ()=> (req,res) => {
+  app.get('*', (req,res) => {
     res.sendFile(path.resolve(__dirname, "client", 'build', 'index.html'))
-  }
+  })
 }
 
 const PORT = process.env.PORT || 5000;
